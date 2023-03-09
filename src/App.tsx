224cb52ab7cee4b832f8e004c1d45b44f9fab4c1
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-03-09 15:24:07 +0300

    Created separate components for loader and errorMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { ErrorMessage } from "./components/ErrorMessage";
+import { Loader } from "./components/Loader";
 import { Product } from "./components/Product";
 import { useProducts } from "./hooks/products";
 
@@ -6,8 +8,8 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <p className="text-center">Loading...</p>}
-      {error && <p className="text-center text-red-600">{error}</p>}
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
