b04afbce987e74c54c62eea7207bc9194da3cac0
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-03-09 13:24:58 +0300

    createElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { type } from "os";
+import React, { createElement as e } from "react";
 
 function App() {
-  return <h1>Hello React</h1>;
+  // return <h1>Hello React</h1>;
+  return e("div", { className: "container" }, [
+    e("h1", { className: "font-bold" }, "Test JSX"),
+    e("button", { className: "" }, "Click me!"),
+  ]);
 }
 
 export default App;
