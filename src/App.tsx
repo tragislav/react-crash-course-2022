b4de0517e2ef63a867d4cada53a8d656e67c8140
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-03-09 13:47:38 +0300

    Worked with ts interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Product } from "./components/Product";
+import { products } from "./data/products";
 
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product product={products[0]} />
+      <Product product={products[1]} />
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,13 @@
-export function Product() {
+import { IProduct } from "../models";
+
+interface ProductProps {
+  product: IProduct;
+}
+
+export function Product(props: ProductProps) {
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-      Product!
+      {props.product.title}
     </div>
   );
 }
