3d2f494e8a64ab0c6d211cf1e4d29f8ebda6df7a
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2023-03-09 13:30:42 +0300

    styled and functional btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { type } from "os";
 import React, { createElement as e } from "react";
 
 function App() {
-  // return <h1>Hello React</h1>;
   return e("div", { className: "container" }, [
-    e("h1", { className: "font-bold" }, "Test JSX"),
-    e("button", { className: "" }, "Click me!"),
+    e("h1", { className: "font-bold", key: 1 }, "Test JSX"),
+    e(
+      "button",
+      {
+        className: "py-2 px-4 border",
+        key: 2,
+        onClick: () => console.log("Clicked!"),
+      },
+      "Click me!"
+    ),
   ]);
 }
 
